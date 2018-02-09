53e710b1a3fb33f3848a3dda11588bccca5915ab
kien300 <k.t.nguyen@cgiar.org>
2018-02-09 14:37:23 +0700

    new

---
+++ whitefly.R
@@ -0,0 +1,7 @@
+library(tidyverse) #general data 
+library(readxl)
+
+setwd("D:/OneDrive - CGIAR/CIAT/OTA/Nami'sData_2.9.18")
+
+cm <- read_excel("Whitefly_data entry_template_Cambodia.xls")
+vn <- read_excel("Whitefly_data entry_template_Vietnam.xls")
